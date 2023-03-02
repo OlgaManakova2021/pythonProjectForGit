b22f8a37e52b80d7b6d9eef58452711df298714b
olgamanakova <manakovaolga@yandex.ru>
2023-03-02 22:29:46 +0300

    add

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import module
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# module.get_set()
+#
+# module.get_txt()
+#
+# print(module.list_doc.doc)
+#
+# print(module.file_doc.doc)
+#
+# print(module.get_binary())
 
+# print(dir(module))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(module.doc.file_doc.doc)
